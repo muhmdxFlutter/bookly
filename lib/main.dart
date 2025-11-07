b24e05879c85f1602baecbfbe19d5f0051a78b7e
Your Name <muhmd_mustafa>
2025-11-07 19:35:56 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+  runApp(Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
