cd221673d2fb4df4f309dee0c11cde7a188e9b7b
Your Name <muhmd_mustafa>
2025-11-09 07:49:43 +0200

    sliding animation with text

--- lib/Features/splash_feature/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash_feature/presentation/views/widgets/splash_view_body.dart
@@ -1,23 +1,54 @@
 import 'package:bookly/Core/utils/assets_data.dart';
+import 'package:bookly/Features/splash_feature/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    animationController.forward();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  void initSlidingAnimation() {
+     animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero).animate(
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+        );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      
+
       children: [
         Image.asset(AssetsData.logo),
-        const SizedBox(height: 20,), 
-         const Text('Read Free Box',style: TextStyle(),textAlign: TextAlign.center,)
-
-
+        const SizedBox(height: 10),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
-} 
+}
+
