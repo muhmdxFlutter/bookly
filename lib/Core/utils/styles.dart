96396e57e5a633c3f2d044b445332c72266e3c1b
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2025-11-10 16:39:03 +0200

    add font family &make colot font with theme dark

--- lib/Core/utils/styles.dart
+++ lib/Core/utils/styles.dart
@@ -3,7 +3,8 @@ import 'package:flutter/widgets.dart';
 abstract class Styles {
 
   static const TextStyle mediumTitle=
-       TextStyle(fontSize: 22, fontWeight: FontWeight.w600);
+       TextStyle(fontSize: 22,
+       fontWeight: FontWeight.w600);
  
 
 
