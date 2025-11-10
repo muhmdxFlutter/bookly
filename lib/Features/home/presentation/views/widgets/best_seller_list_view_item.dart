653dea794af949dfb57c7a1a68b0bfaf0da96dac
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2025-11-10 23:55:55 +0200

    extract book raiting &best seller item done

--- lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,8 +1,9 @@
-
 import 'package:bookly/Core/utils/assets.dart';
 import 'package:bookly/Core/utils/styles.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/book_raiting.dart';
 import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -28,39 +29,45 @@ class BestSellerListViewItem extends StatelessWidget {
             ),
           ),
           const SizedBox(width: 16),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Harry Potter And The Jugle Book',
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),const SizedBox(height: 3,),
-              Text(
-                'Rudyard Kipling',
-                style: TextStyle(
-                  fontSize: 15,
-                  color: const Color.fromARGB(255, 151, 151, 151),
-                  fontWeight: FontWeight.w700,
-                ),
-              ),const SizedBox(height: 3,),
-              Row(
-                children: [
-                  Text(
-                    '19.99 \$',
-                    style: Styles.textStyle20.copyWith(
-                      fontFamily: KGTSectraFine,
-                      fontWeight: FontWeight.bold,
-                    ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child:const Text(
+                    'Harry Potter And The Jugle Book',
+                    style: Styles.textStyle20,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 3),
+               const Text(
+                  'Rudyard Kipling',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color:  Color.fromARGB(255, 151, 151, 151),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(height: 3),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                   Text(
+                      '19.99 ₤',
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: KGTSectraFine,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    BookRaiting(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
---
+++ lib/Features/home/presentation/views/widgets/book_raiting.dart
@@ -0,0 +1,32 @@
+
+import 'package:bookly/Core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BookRaiting extends StatelessWidget {
+  const BookRaiting({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          FontAwesomeIcons.solidStar,
+          size: 16,
+          color: const Color(0xffFFDD4F),
+        ),
+        SizedBox(width: 6),
+        Text('4.8', style: Styles.textStyle16),
+        SizedBox(width: 5),
+        Text(
+          '(245)',
+          style: Styles.textStyle14.copyWith(
+            color: const Color.fromARGB(255, 151, 151, 151),
+          ),
+        ),
+      ],
+    );
+  }
+}
