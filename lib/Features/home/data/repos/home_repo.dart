077b879374e06bea13248e3aa7975e0c5bc6686f
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2025-11-17 12:22:08 +0200

    continue home repo methods & failure file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/Core/errors/Failure.dart';
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  featchBestSellerBooks();
-  featchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> featchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> featchFeaturedBooks();
 }
