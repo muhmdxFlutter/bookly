cddce33a1c89a947e2ea0d6edccd52af59f7a34e
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2025-11-19 09:34:17 +0200

    finish newest books cubit method

--- lib/Features/home/presentation/manager/cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/cubit/newest_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> featchBestSellerBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.featchBestSellerBooks();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.errMessage)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
 }
